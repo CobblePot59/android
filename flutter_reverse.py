40982c0fd83b4b64069699de09dcf4c6d06c015b
CobblePot59 <38343920+CobblePot59@users.noreply.github.com>
2022-11-10 20:55:37 +0100

    Update flutter_reverse.py

--- flutter_reverse.py
+++ flutter_reverse.py
@@ -1,40 +1,37 @@
-import platform
-import os
+import subprocess
+import requests
+from urllib import request as dl
 import codecs
 
 def prerequisites():
-    command = "os.system('java -version')"
-    if not command:
-        print('Please install java')
+    jdk = subprocess.run('java -version', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if jdk.returncode != 0:
+        print('Please verify your java installation')
     else:
         reverse()
 
 def reverse():
     global apk
     apk = input("Enter the name of your apk : ")
-    version = "apktool_2.4.0.jar"
-    apktool = "https://bitbucket.org/iBotPeaches/apktool/downloads/" + version
-
-    if platform.system() == "Windows":
-            os.system('powershell -c Invoke-WebRequest -Uri ' + apktool + ' -OutFile ' + version)
-            os.system('java -jar ' + version +  ' d ' + apk)
-            get_code()
-    else:
-            os.system('curl ' + apktool + ' -o ' + version)
-            os.system('java -jar ' + version +  ' d ' + apk)
-            get_code()
+    r = requests.get('https://api.github.com/repos/iBotPeaches/Apktool/releases/latest')
+    apktool = r.json()['assets'][0]['browser_download_url']
+    
+    print('Downloading...')
+    dl.urlretrieve(apktool, 'apktool.jar')
+    
+    print('\nDecompiling...')
+    subprocess.run('java -jar apktool.jar -f d ' + apk, shell=True)
+    get_code()
 
 def get_code():
     unzip_apk = apk.split('.apk')[0]
-    manifest = open(unzip_apk+'/AndroidManifest.xml', 'r', encoding='utf-8')
-    m_content = manifest.read()
-    name = m_content.split('package=')[1].split(' ')[0].strip('\"')
+    manifest = open(unzip_apk+'/AndroidManifest.xml', 'r', encoding='utf-8').read()
+    name = manifest.split('package=')[1].split(' ')[0].strip('\"')
     short_name = name.split('.')[2]
 
     code = codecs.open('code.txt', 'a', 'ANSI')
-    ifile = open(unzip_apk+'/assets/flutter_assets/kernel_blob.bin', 'r', encoding='ANSI')
-    k_content = ifile.read()
-    filter1 = k_content.split(short_name+'/lib')
+    ifile = open(unzip_apk+'/assets/flutter_assets/kernel_blob.bin', 'r', encoding='ANSI').read()
+    filter1 = ifile.split(short_name+'/lib')
 
     i=1
     while i < (len(filter1)-1):
