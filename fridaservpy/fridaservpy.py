e5a3947da40f81005ce8221948a871dbd8b0e9d9
CobblePot59 <38343920+CobblePot59@users.noreply.github.com>
2024-09-01 13:52:47 +0200

    Update fridaservpy.py

--- fridaservpy/fridaservpy.py
+++ fridaservpy/fridaservpy.py
@@ -42,8 +42,8 @@ subprocess.run('adb push tmp/frida-server /data/local/tmp', shell=True, stdout=s
 subprocess.run('adb shell "chmod 755 /data/local/tmp/frida-server"', shell=True)
 
 # Launch frida-server in background
-print('\nfrida-server is started, you can press Ctrl+C')
+print('\nfrida-server is started.')
 subprocess.run('adb shell "nohup /data/local/tmp/frida-server > /dev/null 2>&1 &"', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 # Get frida connected device
-print('Device connected : '+str(frida.get_usb_device()))
+print(f'Device connected : {frida.get_usb_device()}')
